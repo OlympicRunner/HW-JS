41b74fa3ba167e2ca4519f62d813c8c21c20e494
Rostyslav <noviyrost@gmail.com>
2022-08-17 16:41:32 +0300

    i think

--- DZ6/Задача 2/object-array-filter.js
+++ DZ6/Задача 2/object-array-filter.js
@@ -1,12 +1,15 @@
 function filter(objects, ourValue, ourSetting) {
 
-    for (let obj of objects) {
-        for (let [key, value] of Object.entries(obj)) {
-            if (value === ourValue && key === ourSetting) {
-                console.log(obj)
-            }
-        }
+    for (let i = 0; i < objects.length; i++) {
+        let obj = Object.entries(objects[i])
+        
+        console.log(obj[0].includes( ourValue))
+
+        
+        
     }
+
+
 }
 
 let objects = [
@@ -24,4 +27,13 @@ let show = filter(objects, ourValue, ourSetting)
 
 // for (let [key, value] of entries) {
 //     console.log(`${key}: ${value}`)
+// }
+
+
+// for (let obj of objects) {
+//     for (let [key, value] of Object.entries(obj)) {
+//         if (value === ourValue && key === ourSetting) {
+//             console.log(obj)
+//         }
+//     }
 // }
